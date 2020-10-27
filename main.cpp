796dc183b25c0b7712ae14c4ed4f68dbad0e1052
francesco-borzacchiello <fran.borzacchiello@studenti.unina.it>
2020-10-27 12:36:47 +0100

    added features: -sum -subtraction

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int sum(int, int);
+int subtraction(int, int);
+
+int main(int argc, char const *argv[]) {
+    
+    return 0;
+}
+
+
+int sum(int x, int y)
+{
+    return (x + y);
+}
+
+int subtraction(int x, int y)
+{
+    return (x - y);
+}
